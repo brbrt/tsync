dfae08bc2550788b2d767dedf596f138b8008232
brbrt <bencsr@freemail.hu>
2015-07-26 09:08:42 +0200

    Add process running ability

---
+++ runprocess.js
@@ -0,0 +1,41 @@
+var q = require('q');
+var spawn = require('child_process').spawn;
+
+var log = require('./log.js');
+
+
+module.exports = run;
+
+function run(proc, args) {
+	log.debug('Running process: ' + proc  + ' '+ args);
+
+	var deferred = q.defer();
+
+    var proc = spawn(proc, args);
+
+	var out = '', err = '';
+
+	proc.stdout.on('data', function onStdOut(data) {
+	    out += data;
+	});
+
+	proc.stderr.on('data', function onStdErr(data) {
+	    err += data;
+	});
+
+	proc.on('error', function onError(err) {
+		deferred.reject(err);
+	});
+
+	proc.on('close', function onClose(code) {
+        log.debug('Process returned with code: ' + code);
+
+		if (code === 0) {
+			deferred.resolve(out);
+		} else {
+			deferred.reject(err);
+		}
+	});
+
+	return deferred.promise;
+}
--- tsync.js
+++ tsync.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 
 var log = require('./log.js');
+var runprocess = require('./runprocess.js');
 
 log.info('Synchronization started.');
 
-log.debug('sync!');
+runprocess('ls', ['-la'])
+    .then(success)
+    .catch(error)
+    ;
 
-log.info('Synchronization finished.');
+
+function success(data) {
+    log.info('Data: ' + data);
+    log.info('Synchronization finished successfully.');
+}
+
+function error(err) {
+    log.error('Error: ' + err);
+    log.info('Synchronization finished with error.');
+}
 
